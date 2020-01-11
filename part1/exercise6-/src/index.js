ccd69664034475059828f7600a70ad6c27e4e45d
Donald <ediagee@yahoo.com>
2020-01-11 10:36:38 +0200

    exercise 1.8

--- part1/exercise6-/src/index.js
+++ part1/exercise6-/src/index.js
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Statistics = ({ handleClick, good, bad, neutral }) => {
+  return (
+    <div style={{ marginTop: "15px" }}>
+      Give Feedback
+      <div style={{ marginTop: "15px", marginBottom: "20px" }}>
+        <button onClick={() => handleClick("good")}> Good</button>
+        <button onClick={() => handleClick("neutral")}> Neutral</button>
+        <button onClick={() => handleClick("bad")}> Bad</button>
+      </div>
+      Statistics
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {good + neutral + bad}</p>
+      <p>Average {(good + neutral + bad) / 3}</p>
+      <p>Positive {good > 0 ? (good / (good + neutral + bad)) * 100 : 0}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
@@ -18,21 +38,27 @@ const App = () => {
   };
 
   return (
-    <div style={{ marginTop: "15px" }}>
-      Give Feedback
-      <div style={{ marginTop: "15px", marginBottom: "20px" }}>
-        <button onClick={() => handleClick("good")}> Good</button>
-        <button onClick={() => handleClick("neutral")}> Neutral</button>
-        <button onClick={() => handleClick("bad")}> Bad</button>
-      </div>
-      Statistics
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {good + neutral + bad}</p>
-      <p>Average {(good + neutral + bad) / 3}</p>
-      <p>Positive {good > 0 ? (good / (good + neutral + bad)) * 100 : 0}%</p>
-    </div>
+    <Statistics
+      handleClick={handleClick}
+      good={good}
+      bad={bad}
+      neutral={neutral}
+    />
+    // <div style={{ marginTop: "15px" }}>
+    //   Give Feedback
+    //   <div style={{ marginTop: "15px", marginBottom: "20px" }}>
+    //     <button onClick={() => handleClick("good")}> Good</button>
+    //     <button onClick={() => handleClick("neutral")}> Neutral</button>
+    //     <button onClick={() => handleClick("bad")}> Bad</button>
+    //   </div>
+    //   Statistics
+    //   <p>Good {good}</p>
+    //   <p>Neutral {neutral}</p>
+    //   <p>Bad {bad}</p>
+    //   <p>All {good + neutral + bad}</p>
+    //   <p>Average {(good + neutral + bad) / 3}</p>
+    //   <p>Positive {good > 0 ? (good / (good + neutral + bad)) * 100 : 0}%</p>
+    // </div>
   );
 };
 
