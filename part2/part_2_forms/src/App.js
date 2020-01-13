24dbecbb4544e17385528c99a9072821520e208f
Donald <ediagee@yahoo.com>
2020-01-13 20:20:05 +0200

    exeercise 2.8 complete

--- part2/part_2_forms/src/App.js
+++ part2/part_2_forms/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState(null);
 
   const handleSubmitForm = event => {
     event.preventDefault();
@@ -13,16 +14,24 @@ const App = () => {
       }
     }
 
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
 
   const handleSubmitNewName = event => {
     setNewName(event.target.value);
   };
+  const handleSubmitNewNumber = event => {
+    setNewNumber(event.target.value);
+  };
 
   const showList = () =>
-    persons.map(person => <div key={person.name}>{person.name}</div>);
+    persons.map(person => (
+      <div key={person.name}>
+        {person.name} {person.number}
+      </div>
+    ));
 
   return (
     <div>
@@ -31,6 +40,10 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleSubmitNewName} />
         </div>
+        <div>
+          number: <input value={newNumber} onChange={handleSubmitNewNumber} />
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
